8b8e9a4a1a47eb4a3362abbbb70a15ec9a106888
watabe <kazuki137@gmail.com>
2022-04-18 20:50:48 +0900

    Revert "test"
    This reverts commit 3e1048c8081fb9c6b7fa6b0d4af63745659379c3.

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/login.css"  media="print" onload="this.media='all'">
     <title>ログインページ</title>
 </head>
 <body>
     <h2>ログイン</h2>
+
     <form action="" method="post">
         @csrf
         名前：<input type="text" name="name">
@@ -16,13 +16,9 @@
         <input type="submit" value="ログイン">
     </form>
 
-    <dl>
-        <div>
-            <dt>柴犬</dt>
-            <dd>柴犬は、日本原産の日本犬の一種。<br>日本犬の中で唯一の小型犬です。<br>日本の天然記念物に指定された7つの日本犬種の1つです。</dd>
-        </div>
-        </dl>
+
 <h5>testのやつです</h5>
     <a href="register">新規登録</a>
+    {{ dd($filtered) }}
 </body>
 </html>
